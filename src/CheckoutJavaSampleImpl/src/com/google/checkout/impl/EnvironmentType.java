db1ca4f242bf6867092f0507226a50fdcd120ec2
kwhsim <kwhsim@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-03-07 03:50:42 +0000

    Environment type enumeration.

---
+++ src/CheckoutJavaSampleImpl/src/com/google/checkout/impl/EnvironmentType.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.google.checkout.impl;
+
+/**
+ * @author ksim
+ *
+ */
+
+public class EnvironmentType {
+	/** <summary>Use the Sandbox account to post the messages</summary> */
+	public static final int Sandbox = 0;
+	
+	/** <summary>Use the Production account to post the messages</summary> */
+    public static final int Production = 1;
+    
+    /** <summary>Unknown account.</summary> */
+    public static final int Unknown = 2;
+}
