836db8f2328c855aaa498de457e022e7d362bc61
kwhsim <kwhsim@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-03-07 04:50:37 +0000

    Test...

---
+++ src/CheckoutJavaSampleImpl/src/com/google/checkout/test/impl/SampleTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.google.checkout.test.impl;
+
+/**
+ * @author ksim
+ *
+ */
+
+public class SampleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Success!");
+		return;
+	}
+
+}
