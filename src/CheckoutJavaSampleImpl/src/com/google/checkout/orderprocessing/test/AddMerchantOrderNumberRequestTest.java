341b041782d5c6b4669dae6737fea439a3041160
kwhsim <kwhsim@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-03-08 06:25:35 +0000

    Extra Test File...

---
+++ src/CheckoutJavaSampleImpl/src/com/google/checkout/orderprocessing/test/AddMerchantOrderNumberRequestTest.java
@@ -0,0 +1,21 @@
+package com.google.checkout.orderprocessing.test;
+
+import com.google.checkout.orderprocessing.impl.AddMerchantOrderNumberRequestImpl;
+
+public class AddMerchantOrderNumberRequestTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		AddMerchantOrderNumberRequestImpl amonri = 
+			new AddMerchantOrderNumberRequestImpl("123", "abc", "0", "1234567", "999");
+		
+		System.out.println(amonri.getXml());
+		
+		System.out.println("Success!");
+		return;
+	}
+
+}
