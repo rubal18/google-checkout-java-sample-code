75ca5fdd3884dad051b471a60f84f8096e1618ac
inder123 <inder123@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-04-25 00:13:53 +0000

    fixed formatting. Made member variables hidden

--- examples/JavaCheckoutExampleCommon/src/com/google/checkout/example/Message.java
+++ examples/JavaCheckoutExampleCommon/src/com/google/checkout/example/Message.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (C) 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -22,53 +22,52 @@ import java.util.Date;
 import com.google.checkout.util.Utils;
 
 public class Message implements Serializable {
-
-	boolean incoming;
-	Date timestamp;
-	String type;
-	String request;
-	String response;
-	
-	public Message(boolean incoming, Date timestamp, String type, String request, String response) {
-		this.incoming = incoming;
-		this.timestamp = timestamp;
-		this.type = type;
-		this.request = request;
-		this.response = response;
-	}
-	
-	public Date getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	public String getHeader() {
-		String ret = Utils.getDateString(getTimestamp()).replace('T', ' ') + " : " + type;
-		return ret;
-	}
-
-	public String getBody() {
-		StringBuffer sb = new StringBuffer();
-		if (incoming) {
-			sb.append("You Received:\n\n");
-			sb.append(request+"\n");
-			sb.append("You Sent:\n\n");
-			sb.append(response+"\n");
-		}
-		else {
-			sb.append("You Sent:\n\n");
-			sb.append(request+"\n");
-			sb.append("You Received:\n\n");
-			sb.append(response+"\n");			
-		}
-		return sb.toString();
-	}
+  
+  private boolean incoming;
+  private Date timestamp;
+  private String type;
+  private String request;
+  private String response;
+  
+  public Message(boolean incoming, Date timestamp, String type, String request, String response) {
+    this.incoming = incoming;
+    this.timestamp = timestamp;
+    this.type = type;
+    this.request = request;
+    this.response = response;
+  }
+  
+  public Date getTimestamp() {
+    return timestamp;
+  }
+  public void setTimestamp(Date timestamp) {
+    this.timestamp = timestamp;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+  
+  public String getHeader() {
+    String ret = Utils.getDateString(getTimestamp()).replace('T', ' ') + " : " + type;
+    return ret;
+  }
+  
+  public String getBody() {
+    StringBuffer sb = new StringBuffer();
+    if (incoming) {
+      sb.append("You Received:\n\n");
+      sb.append(request+"\n");
+      sb.append("You Sent:\n\n");
+      sb.append(response+"\n");
+    } else {
+      sb.append("You Sent:\n\n");
+      sb.append(request+"\n");
+      sb.append("You Received:\n\n");
+      sb.append(response+"\n");
+    }
+    return sb.toString();
+  }
 }
