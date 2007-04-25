a029e81c788b51fb6a34f7711510c3bfb5322220
inder123 <inder123@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-04-25 00:42:08 +0000

    formatting changes and made the merchantConstants field private

--- examples/JavaCheckoutExampleCommon/src/com/google/checkout/example/notification/OrderStateChangeNotificationProcessorImpl.java
+++ examples/JavaCheckoutExampleCommon/src/com/google/checkout/example/notification/OrderStateChangeNotificationProcessorImpl.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (C) 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -26,36 +26,35 @@ import com.google.checkout.example.GoogleOrder;
 import com.google.checkout.notification.OrderStateChangeNotificationProcessor;
 import com.google.checkout.util.Utils;
 
-public class OrderStateChangeNotificationProcessorImpl extends AbstractNotificationProcessor implements
-		OrderStateChangeNotificationProcessor {
-
-	MerchantConstants merchantConstants;
-	
-	public OrderStateChangeNotificationProcessorImpl(MerchantConstants merchantConstants) {
-		this.merchantConstants = merchantConstants;
-	}
-	
-	public String process(String callbackXML) throws CheckoutException {
-
-		String ack = "";
-		try {
-			Document document = Utils.newDocumentFromString(callbackXML);
-			
-			String orderNumber = Utils.getElementStringValue(document, document.getDocumentElement(), "google-order-number");
-			Date timestamp = Utils.getElementDateValue(document, document.getDocumentElement(), "timestamp");
-			String lastFulStatus = Utils.getElementStringValue(document, document.getDocumentElement(), "new-fulfillment-order-state");
-			String lastFinStatus = Utils.getElementStringValue(document, document.getDocumentElement(), "new-financial-order-state");
-			
-			GoogleOrder order = GoogleOrder.findOrCreate(merchantConstants.getMerchantId(), orderNumber);			
-			order.setLastFulStatus(lastFulStatus);
-			order.setLastFinStatus(lastFinStatus);
-			ack = getAckString();
-			
-			order.addIncomingMessage(timestamp, document.getDocumentElement().getNodeName(), Utils.documentToStringPretty(document), ack);
-		} catch (Exception e) {
+public class OrderStateChangeNotificationProcessorImpl extends AbstractNotificationProcessor
+    implements OrderStateChangeNotificationProcessor {
+  
+  private MerchantConstants merchantConstants;
+  
+  public OrderStateChangeNotificationProcessorImpl(MerchantConstants merchantConstants) {
+    this.merchantConstants = merchantConstants;
+  }
+  
+  public String process(String callbackXML) throws CheckoutException {
+    
+    String ack = "";
+    try {
+      Document document = Utils.newDocumentFromString(callbackXML);
+      
+      String orderNumber = Utils.getElementStringValue(document, document.getDocumentElement(), "google-order-number");
+      Date timestamp = Utils.getElementDateValue(document, document.getDocumentElement(), "timestamp");
+      String lastFulStatus = Utils.getElementStringValue(document, document.getDocumentElement(), "new-fulfillment-order-state");
+      String lastFinStatus = Utils.getElementStringValue(document, document.getDocumentElement(), "new-financial-order-state");
+      
+      GoogleOrder order = GoogleOrder.findOrCreate(merchantConstants.getMerchantId(), orderNumber);
+      order.setLastFulStatus(lastFulStatus);
+      order.setLastFinStatus(lastFinStatus);
+      ack = getAckString();
+      
+      order.addIncomingMessage(timestamp, document.getDocumentElement().getNodeName(), Utils.documentToStringPretty(document), ack);
+    } catch (Exception e) {
       throw new CheckoutException(e);
-		}
-		return ack;
-	}
-
+    }
+    return ack;
+  }  
 }
