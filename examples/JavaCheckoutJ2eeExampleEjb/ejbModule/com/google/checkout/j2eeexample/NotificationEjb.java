36da8b3a40224e7fdefe05d79178440ecc28955b
inder123 <inder123@da7440cc-471d-0410-be28-b1c5e8bd4c72>
2007-04-25 00:48:15 +0000

    formatting changes

--- examples/JavaCheckoutJ2eeExampleEjb/ejbModule/com/google/checkout/j2eeexample/NotificationEjb.java
+++ examples/JavaCheckoutJ2eeExampleEjb/ejbModule/com/google/checkout/j2eeexample/NotificationEjb.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*************************************************/
+ *************************************************/
 package com.google.checkout.j2eeexample;
 
 import javax.jms.TextMessage;
@@ -30,76 +30,75 @@ import com.google.checkout.notification.RiskInformationNotificationProcessor;
  * The Class JmsCallBackProcessorMdb.
  */
 public class NotificationEjb implements javax.ejb.MessageDrivenBean,
-		javax.jms.MessageListener {
-
-	private javax.ejb.MessageDrivenContext messageContext = null;	
-
-	/* (non-Javadoc)
-	 * @see javax.ejb.MessageDrivenBean#setMessageDrivenContext(javax.ejb.MessageDrivenContext)
-	 */
-	public void setMessageDrivenContext(
-			javax.ejb.MessageDrivenContext messageContext)
-			throws javax.ejb.EJBException {
-		this.messageContext = messageContext;
-	}
-
+    javax.jms.MessageListener {
+  
+  private javax.ejb.MessageDrivenContext messageContext = null;
+  
+  /* (non-Javadoc)
+   * @see javax.ejb.MessageDrivenBean#setMessageDrivenContext(javax.ejb.MessageDrivenContext)
+   */
+  public void setMessageDrivenContext(
+      javax.ejb.MessageDrivenContext messageContext)
+      throws javax.ejb.EJBException {
+    this.messageContext = messageContext;
+  }
+  
   /* (non-Javadoc)
    * @see javax.ejb.MessageDrivenBean#ejbCreate()
    */
   public void ejbCreate() {
     messageContext = null;
   }
-
-	/* (non-Javadoc)
-	 * @see javax.ejb.MessageDrivenBean#ejbRemove()
-	 */
-	public void ejbRemove() {
-		messageContext = null;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see javax.jms.MessageListener#onMessage(javax.jms.Message)
-	 */
-	public void onMessage(javax.jms.Message message) {
-		try {
+  
+  /* (non-Javadoc)
+   * @see javax.ejb.MessageDrivenBean#ejbRemove()
+   */
+  public void ejbRemove() {
+    messageContext = null;
+  }
+  
+  /* (non-Javadoc)
+   * @see javax.jms.MessageListener#onMessage(javax.jms.Message)
+   */
+  public void onMessage(javax.jms.Message message) {
+    try {
       String contents = ((TextMessage)message).getText();
-			dispatch(contents);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private String dispatch(String notification) throws Exception {
-
-		if (notification.indexOf("new-order-notification") > -1) {
-			NewOrderNotificationProcessor processor = CheckoutRequestFactory.newNewOrderNotificationProcessor();
-			return processor.process(notification);
-		}
-		if (notification.indexOf("risk-information-notification") > -1) {
-			RiskInformationNotificationProcessor processor = CheckoutRequestFactory.newRiskInformationNotificationProcessor();
-			return processor.process(notification);		
-		}
-		if (notification.indexOf("order-state-change-notification") > -1) {
-			OrderStateChangeNotificationProcessor processor = CheckoutRequestFactory.newOrderStateChangeNotificationProcessor();
-			return processor.process(notification);
-		}
-		if (notification.indexOf("charge-amount-notification") > -1) {
-			ChargeNotificationProcessor processor = CheckoutRequestFactory.newChargeNotificationProcessor();
-			return processor.process(notification);
-		}
-		if (notification.indexOf("refund-amount-notification") > -1) {
-			RefundNotificationProcessor processor = CheckoutRequestFactory.newRefundNotificationProcessor();
-			return processor.process(notification);
-		}
-		if (notification.indexOf("chargeback-amount-notification") > -1) {
-			ChargebackNotificationProcessor processor = CheckoutRequestFactory.newChargeBackNotificationProcessor();
-			return processor.process(notification);
-		}
-		if (notification.indexOf("authorization-amount-notification") > -1) {
-			AuthorizationNotificationProcessor processor = CheckoutRequestFactory.newAuthorizationNotificationProcessor();
-			return processor.process(notification);
-		}
-		throw new Exception("Notification not recoginsed.");
-	}
+      dispatch(contents);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private String dispatch(String notification) throws Exception {
+    
+    if (notification.indexOf("new-order-notification") > -1) {
+      NewOrderNotificationProcessor processor = CheckoutRequestFactory.newNewOrderNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("risk-information-notification") > -1) {
+      RiskInformationNotificationProcessor processor = CheckoutRequestFactory.newRiskInformationNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("order-state-change-notification") > -1) {
+      OrderStateChangeNotificationProcessor processor = CheckoutRequestFactory.newOrderStateChangeNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("charge-amount-notification") > -1) {
+      ChargeNotificationProcessor processor = CheckoutRequestFactory.newChargeNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("refund-amount-notification") > -1) {
+      RefundNotificationProcessor processor = CheckoutRequestFactory.newRefundNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("chargeback-amount-notification") > -1) {
+      ChargebackNotificationProcessor processor = CheckoutRequestFactory.newChargeBackNotificationProcessor();
+      return processor.process(notification);
+    }
+    if (notification.indexOf("authorization-amount-notification") > -1) {
+      AuthorizationNotificationProcessor processor = CheckoutRequestFactory.newAuthorizationNotificationProcessor();
+      return processor.process(notification);
+    }
+    throw new Exception("Notification not recoginsed.");
+  }
 }
